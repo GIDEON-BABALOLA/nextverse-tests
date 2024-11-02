123c38e9a2febf7c69369e47391af4273f276fba
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-11-02 17:24:02 +0100

    still working

--- client/src/components/Dashboard/common/DevelopersCard.jsx
+++ client/src/components/Dashboard/common/DevelopersCard.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 const DevelopersCard = ({ developer }) => {
     const [loading, setLoading] = useState(true)
   
-    const { loaded, error } = useImageLoad(developer.image);
+    const { loaded, error } = useImageLoad(developer.avatar);
     useEffect(() => {
         if (error) {
           console.log("failed to load images")
@@ -18,23 +18,18 @@ const DevelopersCard = ({ developer }) => {
   return (
     <>{
 
- !loaded ? <div>
+ loading ? <div>
  <div className="box">
-    <div style={{display : "flex", flexDirection : "row", width : "100%", alignItems : "center", justifyItems : "center", justifyContent : "center"}}>
-<div className=" skeleton-image-developer-avatar">  </div>
+    <div style={{display : "flex", flexDirection : "column", gap : "6px", width : "100%", alignItems : "center", justifyItems : "center", justifyContent : "center"}}>
+<div className="skeletal-image">  </div>
+<div className="skeletal-name"></div>
+<div className="skeletal-title"></div>
     </div>
-    <h3>{developer.name}</h3>
-    <h5>{developer.role}</h5>
-    <div className="icons">
-    <a href={developer.instagram} target="_blank" rel="noopener noreferrer">
-    <FaInstagram />
-</a>
-<a href={developer.twitter} target="_blank" rel="noopener noreferrer">
-<FaXTwitter />
-</a>
-<a href={developer.linkedin} target="_blank" rel="noopener noreferrer">
-<FaLinkedin />
-</a>
+    <div className="icons facemenow" >
+ <div className="skeletal-icon"></div>
+ <div className="skeletal-icon"></div>
+ <div className="skeletal-icon"></div>
+
       
     </div>
    </div>
