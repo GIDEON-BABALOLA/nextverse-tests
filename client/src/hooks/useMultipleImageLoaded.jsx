c104c4ca1ee07eefb253055b051ec93cf3118cd3
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-11-06 22:55:40 +0100

    still working

--- client/src/hooks/useMultipleImageLoaded.jsx
+++ client/src/hooks/useMultipleImageLoaded.jsx
@@ -13,10 +13,12 @@ const useMultipleImageLoad = (...urls) => {
 
     let isCancelled = false;
 
+  
     const loadImage = (url, index) => {
       const img = new Image();
       img.src = url;
       img.onload = () => {
+        console.log("loaded me")
         if (!isCancelled) {
           return setImageStatus((prevStatus) => 
             prevStatus.map((status, i) => 
@@ -26,6 +28,7 @@ const useMultipleImageLoad = (...urls) => {
         }
       };
       img.onerror = () => {
+        console.log("why")
         if (!isCancelled) {
           return setImageStatus((prevStatus) => 
             prevStatus.map((status, i) => 
@@ -35,54 +38,7 @@ const useMultipleImageLoad = (...urls) => {
         }
       };
     };
-
-    
-    // const loadImage = async (url, index) => {
-    //   try {
-    //     const response = await fetch(url, { method: 'HEAD' }); // Check if image exists
-    //     console.log(response.status)
-    //     if (response.status === 404) {
-    //       console.log("false man")
-    //       console.log(url)
-    //       // If 404, set error state and exit early
-    //       setImageStatus((prevStatus) =>
-    //         prevStatus.map((status, i) =>
-    //           i === index ? { ...status, error: true } : status
-    //         )
-    //       );
-    //       return;
-    //     }
-    
-    //     // If not 404, proceed with loading image
-    //     const img = new Image();
-    //     img.src = url;
-    //     img.onload = () => {
-    //       if (!isCancelled) {
-    //         setImageStatus((prevStatus) =>
-    //           prevStatus.map((status, i) =>
-    //             i === index ? { ...status, loaded: true } : status
-    //           )
-    //         );
-    //       }
-    //     };
-    //     img.onerror = () => {
-    //       if (!isCancelled) {
-    //         setImageStatus((prevStatus) =>
-    //           prevStatus.map((status, i) =>
-    //             i === index ? { ...status, error: true } : status
-    //           )
-    //         );
-    //       }
-    //     };
-    //   } catch (error) {
-    //     console.error("Error checking image status:", error);
-    //     setImageStatus((prevStatus) =>
-    //       prevStatus.map((status, i) =>
-    //         i === index ? { ...status, error: true } : status
-    //       )
-    //     );
-    //   }
-    // };    
+   
     urls.forEach((url, index) => loadImage(url, index));
     return () => {
       isCancelled = true;
@@ -126,3 +82,65 @@ export default useMultipleImageLoad;
 //     }
 //   };
 // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const loadImage = async (url, index) => {
+//   try {
+//     const response = await fetch(url, { method: 'HEAD' }); // Check if image exists
+//     console.log(response.status)
+//     if (response.status === 404) {
+//       console.log("false man")
+//       console.log(url)
+//       // If 404, set error state and exit early
+//       setImageStatus((prevStatus) =>
+//         prevStatus.map((status, i) =>
+//           i === index ? { ...status, error: true } : status
+//         )
+//       );
+//       return;
+//     }
+
+//     // If not 404, proceed with loading image
+//     const img = new Image();
+//     img.src = url;
+//     img.onload = () => {
+//       if (!isCancelled) {
+//         setImageStatus((prevStatus) =>
+//           prevStatus.map((status, i) =>
+//             i === index ? { ...status, loaded: true } : status
+//           )
+//         );
+//       }
+//     };
+//     img.onerror = () => {
+//       if (!isCancelled) {
+//         setImageStatus((prevStatus) =>
+//           prevStatus.map((status, i) =>
+//             i === index ? { ...status, error: true } : status
+//           )
+//         );
+//       }
+//     };
+//   } catch (error) {
+//     console.error("Error checking image status:", error);
+//     setImageStatus((prevStatus) =>
+//       prevStatus.map((status, i) =>
+//         i === index ? { ...status, error: true } : status
+//       )
+//     );
+//   }
+// };   
