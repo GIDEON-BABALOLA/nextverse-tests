163a2c5527328104e8e2f4f228ad68179ca86938
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-11-15 01:35:52 +0100

    still working

--- server/data/mockUsersCorrect.js
+++ server/data/mockUsersCorrect.js
@@ -1,3 +1,122 @@
+
+
+
+
+
 const mockUsers = [
+    {
+"username" : "Folajimi_Temi",
+"email" : "folajimi@gmail.com",
+"password" : "Folajimi11$#123",
+"mobile" : "08149747221"
+    },
+    {
+        "username" : "King_Honour",
+        "email" : "honour@gmail.com",
+        "password" : "Honour11$#123",
+        "mobile" : "08149787222"
+    },
+    {   
+        "username" : "King_Vinicius",
+        "email" : "vinicius@gmail.com",
+        "password" : "Vinicius11$#123",
+        "mobile" : "08149787223"
+    },
+    {
+        "username" : "King_Bellingham",
+        "email" : "bellingham@gmail.com",
+        "password" : "Bellingham11$#123",
+        "mobile" : "08149787224"
+    },
+    {
+        "username" : "King_Rodri",
+        "email" : "rodri@gmail.com",
+        "password" : "Rodri11$#123",
+        "mobile" : "08149787225"
+    },
+    {
+        "username" : "King_Yamal",
+        "email" : "yamal@gmail.com",
+        "password" : "Yamal11$#123",
+        "mobile" : "08149787226"
+    },
+    {
+        "username" : "King_Andrew",
+        "email" : "andrew@gmail.com",
+        "password" : "Andrew11$#123",
+        "mobile" : "08149787237"
+    },
+    {
+        "username" : "King_Femi",
+        "email" : "femi24@gmail.com",
+        "password" : "Femi11@#$123",
+        "mobile" : "08149787228"
+    },
+    {
+        "username" : "King_Kayode",
+        "email" : "kayode24@gmail.com",
+        "password" : "Kayode11@#$123",
+        "mobile" : "08149767221"
+    },
+    {
+        "username" : "King_Jacob",
+        "email" : "jacob@gmail.com",
+        "password" : "Jacob11$#123",
+        "mobile" : "08149787212"
+    },
+    {   
+        "username" : "King_Isaac",
+        "email" : "isaac@gmail.com",
+        "password" : "Vinicius11$#123",
+        "mobile" : "08149787213"
+    },
+    {
+        "username" : "King_Melchizedek",
+        "email" : "melchizedek@gmail.com",
+        "password" : "Bellingham11$#123",
+        "mobile" : "08149787214"
+    },
+    {
+        "username" : "King_David",
+        "email" : "david@gmail.com",
+        "password" : "Rodri11$#123",
+        "mobile" : "08149787215"
+    },
+    {
+        "username" : "King_Samuel",
+        "email" : "samuel@gmail.com",
+        "password" : "Yamal11$#123",
+        "mobile" : "08149787216"
+    },
+    {
+        "username" : "King_Herold",
+        "email" : "herold@gmail.com",
+        "password" : "Andrew11$#123",
+        "mobile" : "08149787217"
+    },
+    {
+        "username" : "sheyifunmi",
+        "email" : "sheyifunmi@gmail.com",
+        "password" : "Femi11@#$123",
+        "mobile" : "08149787218"
+    },
+    {
+        "username" : "tolani",
+        "email" : "tolani@gmail.com",
+        "password" : "Kayode11@#$123",
+        "mobile" : "08149787221"
+    },
+    {
+        "username" : "Gidiboy",
+        "email" : "gideonbabalola69@gmail.com",
+        "password" : "Gcomputers11$123",
+        "mobile" : "08142787227"
+            },
+    {
+                "username" : "David_writes",
+                "email" : "david69@gmail.com",
+                "password" : "Gcomputers11$123",
+                "mobile" : "08142687227"
+                    },
 ]
 module.exports = { mockUsers }
