0519d29302f76e185d2532fc1cc262a95fe50fa9
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-12-22 14:42:10 +0100

    Lets go

--- client/src/components/Feed/FeedBottomNav.jsx
+++ client/src/components/Feed/FeedBottomNav.jsx
@@ -4,55 +4,53 @@ import { FaHome } from "react-icons/fa"
 import { Link } from "react-router-dom"
 import FeedAvatar from "./FeedAvatar"
 import { useAuthContext } from "../../hooks/useAuthContext"
+import useNavigatePage from "../../hooks/useNavigatePage"
 const FeedBottomNav = ({ view, changeView }) => {
     const { user } = useAuthContext()
+    const navigatePage = useNavigatePage();
   return (
     <div className="galacticus">
    <div className="phone-feed-sidebar-menu">
 <ul className="phone-feed-sidebar-list">
     <li className="phone-feed-sidebar-item">
-<Link className="phone-feed-sidebar-nav-link" to={"/dashboard/profile"}>
+<span className="phone-feed-sidebar-nav-link" 
+      onClick={() => { navigatePage(`/profile/${user["username"]}`)}} 
+>
 <FeedAvatar
          image={user["picture"]}
          className="feed-man"
          alt="Author"
           />
 <span className="phone-feed-sidebar-nav-name">Profile</span>
-</Link>
+</span>
     </li>
     <li className="phone-feed-sidebar-item">
-<Link className="phone-feed-sidebar-nav-link" to={"/"}>
+<span className="phone-feed-sidebar-nav-link"  onClick={() => { navigatePage("/")}} >
 <FaHome className="phone-feed-sidebar-icon"  color=" var(--icon-color)"/>
 <span className="phone-feed-sidebar-nav-name">Home</span>
-</Link>
+</span>
     </li>
     <li className="phone-feed-sidebar-item">
-<Link className="phone-feed-sidebar-nav-link" to={"/dashboard/bookmarks"}>
+<span className="phone-feed-sidebar-nav-link" onClick={() => { navigatePage("/dashboard/bookmarks")}}>
 
     <MdOutlineBookmarks className="phone-feed-sidebar-icon" color=" var(--icon-color)"/>
 
 
 <span className="phone-feed-sidebar-nav-name">Bookmarks</span>
-</Link>
+</span>
     </li>
     <li className="phone-feed-sidebar-item">
-    <Link className={`phone-feed-sidebar-nav-link ${ view.list && "active-structure"}`} >
+    <span className={`phone-feed-sidebar-nav-link ${ view.list && "active-structure"}`} >
 <MdFormatListBulleted className="phone-feed-sidebar-icon" onClick={changeView}/>
 <span className="phone-feed-sidebar-nav-name">List</span>
-</Link>
-    </li>
-    <li className="phone-feed-sidebar-item">
-<Link className="phone-feed-sidebar-nav-link" to={"/dashboard/stories"}>
-<MdOutlineCreate className="phone-feed-sidebar-icon" color=" var(--icon-color)"/>
-<span className="phone-feed-sidebar-nav-name">Write</span>
-</Link>
+</span>
     </li>
     <li className="phone-feed-sidebar-item ">
-<Link className={`phone-feed-sidebar-nav-link ${ view.grid && "active-structure"}`} >
+<span className={`phone-feed-sidebar-nav-link ${ view.grid && "active-structure"}`} >
 <MdGridView className="phone-feed-sidebar-icon " onClick={changeView}  />
 <span className="phone-feed-sidebar-nav-name">Grid</span>
 {/* <div className="feed-sidebar-icon" style={{background : "#F5F5F5", borderRadius : "50%"}}><MdGridView size={20} /></div> */}
-</Link>
+</span>
     </li>
 </ul>
    </div>
