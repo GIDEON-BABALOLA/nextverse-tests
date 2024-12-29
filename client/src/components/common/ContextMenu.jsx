4fe44213fcebf74aa8df53d4e5405f12034eb5fb
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2024-12-29 21:49:47 +0100

    Let's Keep Going

--- client/src/components/common/ContextMenu.jsx
+++ client/src/components/common/ContextMenu.jsx
@@ -5,21 +5,27 @@ import { useBookmarkAStory } from "../../hooks/useBookmarkAStory"
 import { useUnBookmarkAStory } from "../../hooks/useUnBookmarkAStory"
 import { useLikeAStory } from "../../hooks/useLikeAStory";
 import { useUnLikeAStory } from "../../hooks/useUnlikeAStory";
+import { FaRegBookmark, FaBookmark } from "react-icons/fa"
+import { MdOutlineFavorite, MdOutlineFavoriteBorder } from "react-icons/md"
 import { useState } from "react"
 const ContextMenu = ({ contextMenuData,
     setContextMenu,
     shareModal,
     contextMenu,
     state,
-    currentStoryDetails
+    currentStoryDetails,
+    currentStoryId
 }) => {
     const [likedBefore, setLikedBefore] = useState(currentStoryDetails["isLiked"]);
     const [bookmarkedBefore, setBookmarkedBefore] = useState(currentStoryDetails["isBookmarked"]);
     const [bookmarking, setBookmarking] = useState(false)
-    const bookmarkAStory = useBookmarkAStory();
-    const unBookmarkAStory  = useUnBookmarkAStory();
-    const likeAStory = useLikeAStory();
-    const unlikeAStory = useUnLikeAStory();
+    const [unBookmarking, setUnBookmarking] = useState(false)
+    const [liking, setLiking] = useState(false)
+    const [unLiking, setUnLiking] = useState(false)
+    const bookmarkStory = useBookmarkAStory();
+    const unBookmarkStory  = useUnBookmarkAStory();
+    const likeStory = useLikeAStory();
+    const unlikeStory = useUnLikeAStory();
     const navigate = useNavigate()
     const context = useRef()
     // const triangle = useRef()
@@ -27,7 +33,50 @@ const ContextMenu = ({ contextMenuData,
     useEffect(() => {
         setContextMenu(context)
     }, [setContextMenu])
-
+const likeTheStory = () => {
+    setLiking(true)
+    setLikedBefore(false)
+    likeStory.likeAStory(currentStoryId)
+}
+const unlikeTheStory = () => {
+    setUnLiking(true)
+    setLikedBefore(true)
+    unlikeStory.unlikeAStory(currentStoryId)
+}
+const bookmarkAStory = () => {
+    setBookmarking(true)
+    setBookmarkedBefore(false)
+bookmarkStory.bookmarkAStory(currentStoryId)
+}
+const unBookmarkAStory = () => {
+    setUnBookmarking(true)
+    setBookmarkedBefore(true)
+  unBookmarkStory.unbookmarkAStory(currentStoryId)
+}
+useEffect(() => {
+if(Object.keys(likeStory.data).length  > 0){
+    setLikedBefore(false)
+    setLiking(false)
+}
+      }, [likeStory.data, likeStory.error])
+useEffect(() => {
+if(Object.keys(unlikeStory.data).length  > 0){
+    setLikedBefore(true)
+    setUnLiking(false)
+}
+          }, [unlikeStory.data, unlikeStory.error])
+useEffect(() => {
+ if(Object.keys(bookmarkStory.data).length  > 0){
+              setBookmarkedBefore(false)
+              setBookmarking(false)
+    }
+ }, [bookmarkStory.data])
+useEffect(() => {
+if(Object.keys(unBookmarkStory.data).length  > 0){
+                  setBookmarkedBefore(true)
+                  setUnBookmarking(false)
+                }
+}, [unBookmarkStory.data])
     const openShare = (e) => {
 
         switch (e.target.innerText) {
@@ -61,7 +110,7 @@ const ContextMenu = ({ contextMenuData,
         }
     }
     const renderBookmarkButton  = () => {
-        if (Object.keys(bookmarkAStory.data).length == 0 && !bookmarking && !bookmarkedBefore){
+        if (Object.keys(bookmarkStory.data).length == 0 && !bookmarking && !bookmarkedBefore){
             return (
               <>
               <FaRegBookmark className="phone-feed-sidebar-icon phone-feed-sidebar-nav-link"  onClick={() => bookmarkAStory()}
@@ -70,7 +119,7 @@ const ContextMenu = ({ contextMenuData,
               </>
             )
           }
-          if (bookmarking && !bookmarkAStory.error && !bookmarkedBefore){
+          if (bookmarking && !bookmarkStory.error && !bookmarkedBefore){
             return (
               <>
               <FaBookmark
@@ -80,7 +129,7 @@ const ContextMenu = ({ contextMenuData,
             
             )
           }
-          if (!bookmarking && Object.keys(bookmarkAStory.data).length > 0 && !bookmarkedBefore){
+          if (!bookmarking && Object.keys(bookmarkStory.data).length > 0 && !bookmarkedBefore){
             return (
   
               <>
@@ -93,7 +142,7 @@ const ContextMenu = ({ contextMenuData,
         
             )
           }
-          if (bookmarkAStory.error && !bookmarkedBefore){
+          if (bookmarkStory.error && !bookmarkedBefore){
             return (
               <>
               
@@ -107,13 +156,143 @@ const ContextMenu = ({ contextMenuData,
           }
     }
     const renderUnBookmarkButton = () => {
-
+        if (Object.keys(unBookmarkStory.data).length == 0 && !unBookmarking && bookmarkedBefore){
+            return (
+      
+               <>
+               <FaBookmark
+               size={20}  color="#757575" className="phone-feed-sidebar-icon phone-feed-sidebar-nav-link"
+               onClick={() => unBookmarkAStory()}
+               />
+               <span className="phone-feed-sidebar-nav-name">UnBookmark</span>
+               </>
+        
+            )
+          }
+          if (unBookmarking && !unBookmarkStory.error && bookmarkedBefore){
+            return (
+      
+              <>
+              <FaRegBookmark
+              size={20}  color="#757575" className="phone-feed-sidebar-icon phone-feed-sidebar-nav-link"
+              />
+              <span className="phone-feed-sidebar-nav-name">UnBookmarking</span>
+              </>
+        
+           
+        
+            )
+          }
+          if (!unBookmarking && Object.keys(unBookmarkStory.data).length > 0 && bookmarkedBefore){
+            return(
+    
+              <>
+              <FaRegBookmark
+              size={20}  color="#757575" className="phone-feed-sidebar-icon phone-feed-sidebar-nav-link"
+              onClick={() => bookmarkAStory()}
+              />
+              <span className="phone-feed-sidebar-nav-name">Bookmark</span>
+              </>
+        
+            
+            )
+           
+           
+              
+          }
+          if (unBookmarkStory.error && bookmarkedBefore){
+            return (
+              <>
+              <FaBookmark
+              size={20}  color="#757575" className="phone-feed-sidebar-icon phone-feed-sidebar-nav-link"
+              onClick={() => unBookmarkAStory()}
+              />
+              <span className="phone-feed-sidebar-nav-name">UnBookmark</span>
+              </>
+            )
+          }
     }
     const renderLikeButton = () => {
+        if (Object.keys(likeStory.data).length == 0 && !liking && !likedBefore){
+          return (
 
+            <MdOutlineFavoriteBorder 
+            onClick={() => likeTheStory()}
+            size={20} color="var(--actions-button-color)"/>
+          )
+        }
+        if (liking && !likeStory.error && !likedBefore){
+          return (
+          
+                <MdOutlineFavorite 
+            
+            size={20} color="var(--like-icon)"/>
+       
+          
+          )
+        }
+        if (!liking && Object.keys(likeStory.data).length > 0 && !likedBefore){
+          return (
+  <MdOutlineFavorite
+          onClick={() => unlikeTheStory()}
+           size={20} color="var(--like-icon)"/>
+  
+          )
+        }
+        if (likeStory.error && !likedBefore){
+          return (
+            <MdOutlineFavoriteBorder
+            onClick={() => likeTheStory()}
+             size={20} color="var(--actions-button-color)"/>
+          )
+        }
     }
     const renderUnLikeButton = () => {
-
+        if (Object.keys(unlikeStory.data).length == 0 && !unLiking && likedBefore){
+            return (
+  
+  
+  <MdOutlineFavorite
+              onClick={() => unlikeTheStory()}
+               size={20} color="var(--like-icon)"/>
+  
+  
+    
+            )
+          }
+          if (unLiking && !unlikeStory.error && likedBefore){
+            return (
+              
+  <MdOutlineFavoriteBorder 
+              size={20} color="var(--actions-button-color)"/>
+  
+  
+  
+           
+        
+            )
+          }
+          if (!unLiking && Object.keys(unlikeStory.data).length > 0 && likedBefore){
+            return(
+              
+            <MdOutlineFavoriteBorder 
+               onClick={() => likeTheStory()}
+              size={20} color="var(--actions-button-color)"/>
+         
+  
+            
+            )
+           
+           
+              
+          }
+          if (unlikeStory.error && likedBefore){
+            return (
+              <MdOutlineFavorite
+              onClick={() => unlikeTheStory()}
+               size={20} color="var(--like-icon)"/>
+            )
+          }
     }
   return (
     <>    
