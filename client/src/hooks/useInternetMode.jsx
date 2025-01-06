7fb347e825839d25f40f6b0ce2b23777886fd247
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-01-06 13:23:59 +0100

    I will finish today

--- client/src/hooks/useInternetMode.jsx
+++ client/src/hooks/useInternetMode.jsx
@@ -1,21 +1,42 @@
 import { useState, useEffect } from "react";
+import axios from "axios"
 const useInternetMode = () => {
     const [internetMode, setInternetMode] = useState({
         online : /*navigator.onLine*/ undefined,
         offline : /*!navigator.onLine*/ undefined
     })
+
     useEffect(() => {
-  const handleOnline = () => {
-    setInternetMode({
+    const checkInternetAccess = async  () => {
+      let internetAccess;
+      try{
+const response = await axios.head("https://jsonplaceholder.typicode.com/todos/1")
+if (response.status >= 200 && response.status < 300) {
+internetAccess = true
+}
+      }catch(error){
+internetAccess = false
+      }
+      return internetAccess
+    }
+  const handleOnline = async () => {
+    const isOnline = await checkInternetAccess(1)
+    if(isOnline){
+      setInternetMode({
         online : true,
         offline : false
-    })
+    }) 
+    }
   }
-  const handleOffline = () => {
-    setInternetMode({
+  const handleOffline = async () => {
+    const isOffline = await checkInternetAccess(1)
+    if(!isOffline){
+      setInternetMode({
         online : false,
         offline : true
     })
+    }
+ 
   }
   window.addEventListener("online", handleOnline)
   window.addEventListener("offline", handleOffline)
