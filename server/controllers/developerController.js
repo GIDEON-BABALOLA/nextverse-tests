b4ac8c02a021990f356472b86b0fa14fd89fbfa4
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-02-15 09:21:22 +0100

    Liverpool will win the premier league

--- server/controllers/developerController.js
+++ server/controllers/developerController.js
@@ -14,6 +14,7 @@ const { developerConfirmationArray, hashDeveloperEmail }= require(path.join(__di
 const { avatars } = require(path.join(__dirname, "..", "data", "avatars"))
 
 const signupDeveloper = async (req, res) => {
+    console.log("welcome")
     const { username, email, password, mobile, instagram, linkedin, twitter, title} = req.body;
 try{
     //
