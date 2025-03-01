a2c87cb4bb3701672894b8027b7ef72b42bc8798
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-03-01 21:47:14 +0100

    IT

--- server/testcode.js
+++ server/testcode.js
@@ -1,7 +1,8 @@
 console.log("This is where I check if new codes are working")
 console.log("Test file, Here")
 console.log("Interswitch pls reply me")
-console.log("lets go")
+console.log("lets go, come on man")
+
 const formatSize = (bytes) => {
     if (bytes < 1024) return `${bytes} B`;
     if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(2)} KB`;
